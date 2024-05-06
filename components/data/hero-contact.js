ab3bad49d7c693efd0a37db08727503d0d002ae6
gilyenrares <57135053+gilyenrares@users.noreply.github.com>
2024-05-06 10:10:22 +0100

    more data

---
+++ components/data/hero-contact.js
@@ -0,0 +1,31 @@
+export const contactObjOne = {
+    hasTop: false,
+    hasBottom: true,
+    lightBg: false,
+    topLine: 'SIGN UP TODAY',
+    headline: 'Join today to receive an exclusive offer',
+    description:
+      'Get access to our exclusive diamond card that allows you to  send unlimited transactions without getting charged any fees',
+    buttonLink: '/about',
+    buttonLabel: 'Get Started',
+    buttonStyle: 'btn--primary',
+    imgStart: '',
+    img: 'images/svg-6.svg',
+    alt: 'Credit Card'
+  };
+  
+  export const contactObjTwo = {
+    hasTop: true,
+    hasBottom: true,
+    lightBg: false,
+    topLine: 'SIGN UP TODAY',
+    headline: 'Join today to receive an exclusive offer',
+    description:
+      'Get access to our exclusive diamond card that allows you to  send unlimited transactions without getting charged any fees',
+    buttonLink: '/about',
+    buttonLabel: 'Get Started',
+    buttonStyle: 'btn--primary',
+    imgStart: '',
+    img: 'images/svg-6.svg',
+    alt: 'Credit Card'
+  };
---
+++ components/data/hero-index.js
@@ -0,0 +1,35 @@
+export const homeObjOne = {
+    hasTop: false,
+    hasBottom: true,
+    lightBg: false,
+    topLine: 'WELCOME TO RBG DEVELOPMENT',
+    headline: 'About RBG',
+    description:{
+      p1:'On the front end of web development in order to create the overall layout of the website, HTML & CSS are used and combined. Furthermore, JavaScript more specifically jQuery, is used to animate and smooth out the website design, thus creating a pleasant user experience.',
+      p2:' On the back end of web development, PHP and MySql are used to bring functionality into the website and sort and store relevant information in databases.'
+    },
+    buttonLink: '/about',
+    buttonLabel: 'Get Started',
+    buttonStyle: 'btn--primary',
+    imgStart: '',
+    img: 'images/a1.png',
+    alt: 'Confidential'
+  };
+  
+  export const homeObjTwo = {
+    hasTop: false,
+    hasBottom: true,
+    lightBg: true,
+    topLine: '100% SECURE',
+    headline: 'Stay protected 24/7 anywhere anytime',
+     description:{
+      p1:'On the front end of web development in order to create the overall layout of the website, HTML & CSS are used and combined. Furthermore, JavaScript more specifically jQuery, is used to animate and smooth out the website design, thus creating a pleasant user experience.',
+      p2:' On the back end of web development, PHP and MySql are used to bring functionality into the website and sort and store relevant information in databases.'
+    },
+    buttonLink: '/about',
+    buttonLabel: 'Get Started',
+    buttonStyle: 'btn--primary',
+    imgStart: '',
+    img: 'images/svg-5.svg',
+    alt: 'Vault'
+  };
