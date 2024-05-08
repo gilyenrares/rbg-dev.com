e6d71ed17fc7d7f96f670460c8488485328c814d
gilyenrares <57135053+gilyenrares@users.noreply.github.com>
2024-05-08 14:34:27 +0100

    sign up

---
+++ app/sign-up/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './sign-up.css'
+
+const Signup = () => {
+  return (
+    <>
+      <div className='img-header h-10'></div>
+      <div className='img-body h-32'>
+
+      </div>
+      <div className='img-footer h-10'></div>
+    </>
+  )
+}
+
+export default Signup
