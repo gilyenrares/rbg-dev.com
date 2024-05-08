5061960a411b7d0545b8031c731727db3e37d30b
gilyenrares <57135053+gilyenrares@users.noreply.github.com>
2024-05-08 15:23:21 +0100

    forms

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import './sign-up.css'
+import { SignUpForm } from '@/forms'
 
-const Signup = () => {
+const SignUp = () => {
   return (
     <>
       <div className='img-header h-10'></div>
       <div className='img-body h-32'>
-
+        <SignUpForm />
       </div>
       <div className='img-footer h-10'></div>
     </>
   )
 }
 
-export default Signup
+export default SignUp
